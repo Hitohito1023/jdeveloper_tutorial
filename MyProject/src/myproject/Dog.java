8363f1d81254d5d227e4fb95fb559109532c6db6
ryota <ryotasephiroth1023@gmail.com>
2023-03-25 18:42:24 +0900

    finish 13

--- MyProject/src/myproject/Dog.java
+++ MyProject/src/myproject/Dog.java
@@ -1,19 +1,46 @@
 package myproject;
 
-public class Dog {
+public class Dog implements IntAnimal {
+    String name = "Rex";
+    int age;
+
     public Dog() {
         super();
     }
 
-    public String sayHi() {
-        return " woof " + "Jeff";
+    public String sayHowDoYouDo(String name) {
+        return " woof " + name;
     }
 
     public static void main(String[] args) {
+        //        Cat myCat = new Cat();
+        //TODO create a Cat class
         Dog myDog = new Dog();
         for (int count = 0; count < 3; count++) {
-            System.out.println(count + myDog.sayHi());
+            System.out.println(count + myDog.sayHowDoYouDo("Jeff"));
         }
 
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
